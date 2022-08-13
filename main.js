7689594c50df732ffa6a3649df9a852dc4cb79ba
karina Administradora <khuamani2022@hotmail.com>
2022-08-13 16:58:13 -0500

    Menu Dinamicos

--- main.js
+++ main.js
@@ -1,7 +1,33 @@
 const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
-menuEmail.addEventListener('click', alternarMenu);
+const menuHamIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 
-function alternarMenu() {
+const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
+
+menuEmail.addEventListener('click', alternarDesktopMenu);
+menuHamIcon.addEventListener('click', alternMobileMenu);
+menuCarritoIcon.addEventListener('click', alternCarritoAside);
+
+function alternarDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function alternMobileMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
+    mobileMenu.classList.toggle('inactive');
+}
+
+function alternCarritoAside() {
+    // aside.classList.toggle('inactive');
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    }
+    aside.classList.toggle('inactive');
+
 }
